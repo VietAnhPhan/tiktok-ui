bcd856e3ef1f1aa25bd0ab4a085d8989f8aca902
Viet Anh Phan <vietanhphan2810@gmail.com>
2026-09-19 11:46:01 +0700

    set up layout

---
+++ src/components/Layout/DefaultLayout/index.jsx
@@ -0,0 +1,12 @@
+import Header from '../components/Header';
+
+function DefaultLayout() {
+    return (
+        <>
+            <Header />
+            <h1>Default Layout</h1>
+        </>
+    );
+}
+
+export default DefaultLayout;
